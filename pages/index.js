0e30333ad9f512a9895ec2b7f63ac338c9fd7245
Oktay Shakirov <oktayshakirov@gmail.com>
2024-09-07 02:27:49 +0200

    added crypto ogs and exchanges to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Base from "@layouts/Baseof";
 import { getSinglePage } from "@lib/contentParser";
 import Posts from "@partials/Posts";
+import CryptoOGs from "@partials/CryptoOGs";
+import Exchanges from "@partials/Exchanges";
 import { markdownify } from "@lib/utils/textConverter";
 import FearAndGreedIndex from "@layouts/components/FearAndGreedIndex";
 import Link from "next/link";
 
-const Home = ({ posts }) => {
+const Home = ({ posts, ogs, exchanges }) => {
   return (
     <Base>
       <section>
@@ -26,9 +28,19 @@ const Home = ({ posts }) => {
               AUTHORS
             </Link>
           </div>
-          {markdownify("MARKET MOOD: The Crypto Rollercoaster!", "h3")}
-          <Link href="/tools/fear-and-greed-index">Learn more</Link>
-          <FearAndGreedIndex />
+          <div className="container mb-20">
+            {markdownify("MARKET MOOD: The Crypto Rollercoaster!", "h3")}
+            <Link href="/tools/fear-and-greed-index">Learn more</Link>
+            <FearAndGreedIndex />
+          </div>
+          <div className="container mb-20">
+            {markdownify("CRYPTO LEGENDS", "h2", "mb-8")}
+            <CryptoOGs ogs={ogs} />
+          </div>
+          <div className="container mb-20">
+            {markdownify("CRYPTO EXCHANGES", "h2", "mb-8")}
+            <Exchanges exchanges={exchanges.slice(0, 6)} />
+          </div>
         </div>
       </section>
     </Base>
@@ -39,10 +51,14 @@ export default Home;
 
 export const getStaticProps = async () => {
   const posts = getSinglePage("content/posts");
+  const ogs = getSinglePage("content/crypto-ogs");
+  const exchanges = getSinglePage("content/exchanges");
 
   return {
     props: {
       posts: posts || [],
+      ogs: ogs || [],
+      exchanges: exchanges || [],
     },
   };
 };
