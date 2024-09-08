c11d65d2d1ea3c2383e79d3f9dd1866396c27c41
Oktay Shakirov <oktayshakirov@gmail.com>
2024-09-09 00:55:15 +0200

    updated seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,15 +4,36 @@ module.exports = {
   changefreq: "weekly",
   priority: 0.7,
   sitemapSize: 5000,
-  exclude: ["/secret-page"], // Exclude specific pages (optional)
-  // Define custom routes if necessary
-  // transform: async (config, path) => {
-  //   return {
-  //     loc: path, // The path of the page
-  //     changefreq: config.changefreq,
-  //     priority: config.priority,
-  //     lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-  //     alternateRefs: [],
-  //   }
-  // },
+  autoLastmod: true,
+
+  transform: async (config, path) => {
+    let priority = config.priority;
+    let changefreq = config.changefreq;
+
+    if (path === "/") {
+      priority = 1.0;
+    } else if (path.startsWith("/blog")) {
+      changefreq = "daily";
+      priority = 0.8;
+    } else if (path.startsWith("/tools")) {
+      priority = 0.9;
+    }
+
+    return {
+      loc: path,
+      changefreq,
+      priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: [],
+    };
+  },
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    additionalSitemaps: ["https://thecrypto.wiki/sitemap.xml"],
+  },
 };
