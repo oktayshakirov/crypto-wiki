0b9cd9951b66af538b04e491cc6cda5d539eddd3
Oktay Shakirov <oktayshakirov@gmail.com>
2025-03-04 13:52:20 +0100

    added firebase database for content

---
+++ pages/api/triggerPushNotification.js
@@ -0,0 +1,58 @@
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  const { type, title, content } = req.body;
+
+  if (!type || !title) {
+    return res.status(400).json({ error: "Missing type or title" });
+  }
+
+  if (!["posts", "exchanges", "cryptoOgs"].includes(type)) {
+    return res.status(400).json({
+      error: "Invalid type. Must be 'posts', 'exchanges', or 'cryptoOgs'.",
+    });
+  }
+
+  try {
+    await admin
+      .firestore()
+      .collection(type)
+      .doc(title)
+      .set({
+        title,
+        content: content || "",
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        updatedAt: admin.firestore.FieldValue.serverTimestamp(),
+      });
+
+    const notificationRef = admin.firestore().collection("notifications");
+    await notificationRef.add({
+      title,
+      type,
+      content: content ? content.substring(0, 200) + "..." : "",
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+      read: false,
+    });
+
+    return res
+      .status(200)
+      .json({ message: "Content and notification added successfully" });
+  } catch (error) {
+    console.error("Error adding content:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
