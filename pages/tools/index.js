928cb238aca2bf2df6c925a9877a84f58c37611d
oktayshakirov <oktayshakirov@gmail.com>
2025-03-24 21:38:18 +0100

    minor update

--- pages/tools/index.js
+++ pages/tools/index.js
@@ -53,7 +53,7 @@ const Tools = () => {
         <div className="container max-w-6xl">
           <div className="mb-16 text-center">
             {markdownify("CRYPTO TOOLS", "h1", "h2 mb-4")}
-            <p className="text-lg text-gray-600 dark:text-gray-400">
+            <p className="text-lg text-gray-200 ">
               Access the insights you need to navigate the digital currency
               market confidently
             </p>
