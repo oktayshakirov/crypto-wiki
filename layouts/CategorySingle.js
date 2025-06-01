4c86f76553144095cd84734b80a29f826e454163
Oktay Shakirov <oktayshakirov@gmail.com>
2025-06-01 17:46:50 +0200

    added pagination to categories

---
+++ layouts/CategorySingle.js
@@ -0,0 +1,56 @@
+import config from "@config/config.json";
+import Base from "@layouts/Baseof";
+import Posts from "@partials/Posts";
+import Pagination from "@components/Pagination";
+import { FaArrowLeft } from "react-icons/fa";
+import Link from "next/link";
+
+const CategoryLayout = ({
+  category,
+  posts,
+  authors,
+  cryptoOgs,
+  exchanges,
+  currentPage,
+  totalPages,
+}) => {
+  return (
+    <Base
+      title={`Discover ${category} posts | News, Guides & Analysis - Crypto Wiki`}
+      meta_title={`Discover ${category} posts | News, Guides & Analysis - Crypto Wiki`}
+      description={`Explore the latest Crypto ${category} trends, expert analysis, and detailed guides. Perfect for beginners and enthusiasts looking to deepen their crypto knowledge.`}
+      image="/images/meta-image.png"
+      canonical={`${config.site.base_url}/categories/${category.toLowerCase()}`}
+    >
+      <div className="section">
+        <div className="container mb-8 text-left">
+          <Link href="/categories">
+            <button className="flex items-center">
+              <FaArrowLeft className="mr-2" />
+              All Categories
+            </button>
+          </Link>
+        </div>
+        <div className="container">
+          <h1 className="h2 mb-8 text-center">
+            Showing posts from <span className="text-primary">{category}</span>{" "}
+            category
+          </h1>
+          <Posts
+            posts={posts}
+            authors={authors}
+            cryptoOgs={cryptoOgs}
+            exchanges={exchanges}
+          />
+          <Pagination
+            section={`categories/${category}`}
+            totalPages={totalPages}
+            currentPage={currentPage}
+          />
+        </div>
+      </div>
+    </Base>
+  );
+};
+
+export default CategoryLayout;
