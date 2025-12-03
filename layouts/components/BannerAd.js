f181c57af9838fbda3e28db238e57b1b75cbc70c
oktayshakirov <oktayshakirov@gmail.com>
2025-12-03 14:45:27 +0100

    update

--- layouts/components/BannerAd.js
+++ layouts/components/BannerAd.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import Script from "next/script";
 
 const BannerAd = ({ className = "", style = {}, id }) => {
+  const router = useRouter();
   const [isDevelopment, setIsDevelopment] = useState(false);
   const [uniqueId] = useState(
     () => id || `banner-ad-${Math.random().toString(36).substr(2, 9)}`
@@ -73,7 +75,8 @@ const BannerAd = ({ className = "", style = {}, id }) => {
         />
       </div>
       <Script
-        id={`bitmedia-ad-${uniqueId}`}
+        key={`bitmedia-ad-${uniqueId}-${router.asPath}`}
+        id={`bitmedia-ad-${uniqueId}-${router.asPath}`}
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `!function(e,n,c,t,o,r,d){!function e(n,c,t,o,r,m,d,s,a){s=c.getElementsByTagName(t)[0],(a=c.createElement(t)).async=!0,a.src="https://"+r[m]+"/js/"+o+".js?v="+d,a.onerror=function(){a.remove(),(m+=1)>=r.length||e(n,c,t,o,r,m)},s.parentNode.insertBefore(a,s)}(window,document,"script","692e0776457ec2706b483e16",["cdn.bmcdn6.com"], 0, new Date().getTime())}();`,
